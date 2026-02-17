f600ec203d32ede40d3ae692b2f073d814bad14d
Shameer528303 <shalih21117@gmail.com>
2026-02-17 11:25:27 +0530

    Update variables.tf

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -3,7 +3,7 @@
 variable "aws_region" {
   description = "AWS region for resources"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
 variable "project_name" {
