aa5c371950bd3eb58f5503d8fb96604c5e618089
Shameer528303 <shalih21117@gmail.com>
2026-02-17 13:19:00 +0530

    Update outputs.tf

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,5 +1,76 @@
 # Output values for Terraform configuration
 
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = [for s in aws_subnet.public : s.id]
+}
+
+output "private_subnet_ids" {
+  value = [for s in aws_subnet.private : s.id]
+}
+
+output "azs" {
+  value = local.azs
+}
+
+#ECR repository output
+output "ecr_repository_url" {
+  value = aws_ecr_repository.app.repository_url
+}
+
+output "ecr_repository_name" {
+  value = aws_ecr_repository.app.name
+}
+
+#EKS cluster output
+output "eks_cluster_name" {
+  value = aws_eks_cluster.this.name
+}
+
+output "eks_cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+
+output "eks_cluster_ca" {
+  value     = aws_eks_cluster.this.certificate_authority[0].data
+  sensitive = true
+}
+
+output "eks_nodegroup_name" {
+  value = aws_eks_node_group.default.node_group_name
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # TODO: Define outputs for important resources
 # Examples:
 
