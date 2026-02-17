1fbe9a000280ff3bee89a5c96ce24d8258ffeb7a
Shameer528303 <shalih21117@gmail.com>
2026-02-17 17:58:48 +0530

    Update test_health.py

--- app/tests/test_health.py
+++ app/tests/test_health.py
@@ -1,8 +1,10 @@
 from fastapi.testclient import TestClient
-from app import app
+#from app import app
+from main import app
 
 client = TestClient(app)
 
 def test_health():
     r = client.get("/health")
     assert r.status_code in (200, 503)
+
